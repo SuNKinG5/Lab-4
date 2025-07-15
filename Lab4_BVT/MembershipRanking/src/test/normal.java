589de99f9574588cf7d5e3fc06b8c08565a74c22
SuNKinG5 <104212708+SuNKinG5@users.noreply.github.com>
2025-07-15 15:00:19 +0700

    Update1

---
+++ Lab4_BVT/MembershipRanking/src/test/normal.java
@@ -0,0 +1,65 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import sqa.main.Ranking;
+
+class normal {
+
+	Ranking rank = new Ranking();
+	
+	@Test
+	void T1() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T2() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T3() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T4() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T5() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T6() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T7() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T8() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T9() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T10() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T11() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T12() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+	@Test
+	void T13() {
+		assertEquals("Standard", rank.CalculateMembershipRank(0, 16, 500));
+	}
+}
---
+++ Lab4_BVT/MembershipRanking/src/test/robustness.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class robustness {
+	
+	@BeforeAll
+	void beforeAll() {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
